b4a9493e75464f79df0ede31c1841c0841695ea9
Tomáš Macák <50169298+TomasMacak@users.noreply.github.com>
2020-08-14 16:41:50 +0200

    cleared

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class MainWindow(QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.layout = QHBoxLayout()
-        self.textArea = QTextEdit(str(8999999999**899))
+        self.textArea = QTextEdit(str(8999999999**850))
         self.barCSS = (
             "QScrollBar::handle {border-radius:4px;background-color:#616161;width:8px;}" + 
             "QScrollBar::handle:hover {background-color:#bdbdbd;}" + 
